1509f2ce726a17647d8210b8162284307f6b13d8
Hector <info@hootei.es>
2021-10-08 10:53:53 +0200

    PC SMT Catalog

--- pc_smt_catalog/controllers/main.py
+++ pc_smt_catalog/controllers/main.py
@@ -5,6 +5,7 @@ from odoo.exceptions import ValidationError, AccessError, MissingError, UserErro
 from odoo.http import content_disposition, Controller, request, route
 from odoo.tools import consteq
 from odoo.addons.portal.controllers.portal import CustomerPortal
+from odoo.addons.website_sale.controllers.main import WebsiteSale
 from datetime import datetime
 
 import logging
@@ -12,6 +13,15 @@ import logging
 # from odoo.addons.website_mass_mailing.controllers.main import MassMailController
 
 _logger = logging.getLogger(__name__)
+
+
+class WebsiteSale(WebsiteSale):
+    @http.route(['/shop/payment'], type='http', auth="public", website=True, sitemap=False)
+    def payment(self, **post):
+        return request.redirect('/shop/cart')
+    @http.route(['/shop/address'], type='http', auth="public", website=True, sitemap=False)
+    def payment(self, **post):
+        return request.redirect('/shop/cart')
 
 
 class OppoForm(Controller):
@@ -27,9 +37,12 @@ class OppoForm(Controller):
                 if valores:
                     now = datetime.now()
                     date_time = now.strftime("%d/%m/%Y - %H:%M:%S")
+                    website_name = request.website.name
 
-                    crm_lead = request.env['crm.lead'].create({
-                        'name': "New Website Opportunity " + date_time
+                    crm_lead = request.env['crm.lead'].sudo().create({
+                        'name': "New mstech website opportunity " + date_time,
+                        'user_id': 10,
+                        'team_id': 2
                     })
                     if crm_lead:
                         message_cart = "CONFIGURED PRODUCTS <br/><br/>"
@@ -40,32 +53,43 @@ class OppoForm(Controller):
                                 breaker = ""
                                 if i > 0:
                                     breaker = "<br/>"
-                                message_cart = message_cart + breaker + "Producto: " + line.product_id.name + " - Qty: " + str(line.product_uom_qty) + "<br/>"
+                                message_cart = message_cart + breaker + "Producto: " + line.product_id.name + " - Qty: " + str(
+                                    line.product_uom_qty) + "<br/>"
 
-                                i = i +1
+                                i = i + 1
                             message_cart = message_cart + "-----------------------------------------"
                         message = message_cart + "<br/>Tell us about current throughput in your production: <br/>" \
-                                  "1. Number of products required in production per year: " + valores.get('products_required') + "<br/>" \
-                                  "2. Required cycle-time per board: " + valores.get('cycle_time') + "<br/>" \
-                                  "3. Number of production shifts: " + valores.get('production_shifts') + "<br/>" \
-                                  "4. How many different types of boards?: " + valores.get('different_types') + "<br/><br/>" \
-                                  "Technical details: <br/>" \
-                                  "1. Description of the application and what are the customers objectives: " + valores.get('description_application') + "<br/>" \
-                                  "2. Board dimensions and carries dimensions containing how many boards: " + valores.get('board_dimensions') + "<br/>" \
-                                  "3. Material(s) types: " + valores.get('materials_types') + "<br/>" \
-                                  "4. Types of Products that will be quoted: " + valores.get('types_products') + "<br/>" \
-                                  "5. Geometry size. L/I/H mm: " + valores.get('geometry_size') + "<br/><br/>" \
-                                  "Please tell us: <br/>" \
-                                  "1. Estimated budget: " + valores.get('estimated_budget') + "<br/>" \
-                                  "2. Estimated closing date: " + valores.get('estimated_closing_date') + "<br/><br/>" \
-                                  "Company details: <br/>" \
-                                  "Company Name: " + valores.get('company_name') + "<br/>" \
-                                  "Name / Surname: " + valores.get('name_surname') + "<br/>" \
-                                  "Email / Phone: " + valores.get('email_phone') + "<br/>"
-                        crm_lead.message_post(body=message)
+                                                 "1. Number of products required in production per year: " + valores.get(
+                            'products_required') + "<br/>" \
+                                                   "2. Required cycle-time per board: " + valores.get(
+                            'cycle_time') + "<br/>" \
+                                            "3. Number of production shifts: " + valores.get(
+                            'production_shifts') + "<br/>" \
+                                                   "4. How many different types of boards?: " + valores.get(
+                            'different_types') + "<br/><br/>" \
+                                                 "Technical details: <br/>" \
+                                                 "1. Description of the application and what are the customers objectives: " + valores.get(
+                            'description_application') + "<br/>" \
+                                                         "2. Board dimensions and carries dimensions containing how many boards: " + valores.get(
+                            'board_dimensions') + "<br/>" \
+                                                  "3. Material(s) types: " + valores.get('materials_types') + "<br/>" \
+                                                                                                              "4. Types of Products that will be quoted: " + valores.get(
+                            'types_products') + "<br/>" \
+                                                "5. Geometry size. L/I/H mm: " + valores.get(
+                            'geometry_size') + "<br/><br/>" \
+                                               "Please tell us: <br/>" \
+                                               "1. Estimated budget: " + valores.get('estimated_budget') + "<br/>" \
+                                                                                                           "2. Estimated closing date: " + valores.get(
+                            'estimated_closing_date') + "<br/><br/>" \
+                                                        "Company details: <br/>" \
+                                                        "Company Name: " + valores.get('company_name') + "<br/>" \
+                                                        "Company Address: " + valores.get('company_address') + "<br/>" \
+                                                        "Name / Surname: " + valores.get('name_surname') + "<br/>" \
+                                                        "Email / Phone: " + valores.get('email_phone') + "<br/>"
+                        crm_lead.sudo().message_post(body=message)
                     return request.redirect('/contactus-thank-you')
             else:
-                #error
+                # error
                 raise UserError("")
 
         return request.redirect('/shop/cart')
